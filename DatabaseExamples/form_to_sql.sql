a4b97df6c8fc90bb9ab2d535fb34736262ebe5bb
Derrick Sherrill <36718839+Derrick-Sherrill@users.noreply.github.com>
2020-03-26 18:13:43 -0400

    Form to SQL Video

---
+++ DatabaseExamples/form_to_sql.sql
@@ -0,0 +1,10 @@
+CREATE TABLE reports(
+   id serial PRIMARY KEY,
+   employeeid INT,
+   accident VARCHAR (50) NOT NULL,
+   details VARCHAR (150) NOT NULL
+);
+
+
+INSERT INTO reports (employeeid, accident, details)
+    VALUES ( , );
