ae74c80294c8b35a631d86ae45473bec3299af22
Derrick Sherrill <36718839+Derrick-Sherrill@users.noreply.github.com>
2020-03-30 19:47:59 -0400

    Update google sheets automatically video

---
+++ DatabaseExamples/Update_google_sheets_automatically.py
@@ -0,0 +1,18 @@
+CREATE TABLE survey(
+   id serial PRIMARY KEY,
+   name VARCHAR (50) NOT NULL,
+   score INT () NOT NULL
+);
+
+
+INSERT INTO survey (name, score)
+    VALUES ('Derrick', 85),
+            ('Mike', 100),
+            ('Kelsey', 54),
+            ('Henry', 95),
+            ('Samual', 65),
+            ('Tim', 89),
+            ('Savanah', 56);
+
+
+SELECT * FROM survey
