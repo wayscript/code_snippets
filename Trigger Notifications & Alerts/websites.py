7f1fe34ad10fc226f90841a91b210965d9b31a39
Derrick Sherrill <36718839+Derrick-Sherrill@users.noreply.github.com>
2020-04-14 18:46:56 -0400

    redo

--- Trigger Notifications & Alerts/websites.py
+++ Trigger Notifications & Alerts/websites.py
@@ -5,7 +5,12 @@ website_2 = 'https://www.youtube.com/channel/UCv1JpM-XII0PMnqoCJfZA2g'
 
 website_list = [website_1, website_2]
 
-
+status_list = []
 for w in website_list:
     r = requests.get(w)
-    r.status
+    code = r.status_code
+    status_list.append(code)
+
+print(status_list)
+
+variables['status_list'] = status_list
