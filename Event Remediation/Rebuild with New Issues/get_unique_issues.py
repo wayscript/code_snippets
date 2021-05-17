8f02740b1c67356fe004eecc41040ccd12c319e7
Derrick Sherrill <36718839+Derrick-Sherrill@users.noreply.github.com>
2021-05-17 13:24:26 -0400

    fix exception pass error

--- Event Remediation/Rebuild with New Issues/get_unique_issues.py
+++ Event Remediation/Rebuild with New Issues/get_unique_issues.py
@@ -5,20 +5,23 @@ headers = {'X-Rollbar-Access-Token' : 󰀂v.25-rollbar-read-key󰀂}
 response = requests.get('https://api.rollbar.com/api/1/items/?status=active&page=1', headers = headers)
 variables['top_items'] = response.json().get('result').get('items')
 
-df = pd.DataFrame(columns = ['id', 'title'])
-for item in variables['top_items']:
-    df = df.append({'id' : item['id'], 'title' : item['title']}, ignore_index = True)
+try:
+    df = pd.DataFrame(columns = ['id', 'title'])
+    for item in variables['top_items']:
+        df = df.append({'id' : item['id'], 'title' : item['title']}, ignore_index = True)
 
-previous_df = pd.read_excel('previous.xlsx')
-new_values = df[~df['title'].isin(previous_df['title']) ]
-print(new_values)
+    previous_df = pd.read_excel('previous.xlsx')
+    new_values = df[~df['title'].isin(previous_df['title']) ]
+    print(new_values)
 
-new_issues_count = len(new_values['title'])
-print(new_issues_count)
-variables['new_issues_count'] = new_issues_count
+    new_issues_count = len(new_values['title'])
+    print(new_issues_count)
+    variables['new_issues_count'] = new_issues_count
 
-if new_issues_count > 0:
-    variables['new_issues'] = new_values['title']
+    if new_issues_count > 0:
+        variables['new_issues'] = new_values['title']
+ except:
+    pass
 
 
 df.to_excel('previous.xlsx')
